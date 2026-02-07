7bcecf5fafd7aec8d8aecc590d41479921edb88d
j__r0d <jkozeal@gmail.com>
2026-02-06 20:23:06 -0700

    Add nameplate text customization options and settings to enhance visibility

--- Core.lua
+++ Core.lua
@@ -47,6 +47,13 @@ local defaults = {
         hideQuickJoinToastButton = false,
         prdShowWithTargetEnemy = false,
         prdShowWithTargetFriendly = false,
+        prdShowTextName = false,
+        
+        -- Custom Name Text Settings
+        prdNameScale = 1.0,
+        prdNameX = 0,
+        prdNameY = 4,
+
         backgroundPanelEnabled = false,
         backgroundPanelLocked = false,
         backgroundPanelColor = {r=0, g=0, b=0, a=0.5},
